9621e96d9ab0e094bdeca1b22067b534284271b3
Ho Cheung <hoc@genebygene.com>
2018-07-16 11:06:15 -0500

    Allow to write the downloaded body to a file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <boost/asio/ssl/stream.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include <boost/optional/optional_io.hpp>
@@ -21,7 +22,7 @@ int main(int argc, char** argv)
     //    try
     //    {
     // Check command line arguments.
-    if(argc != 4 && argc != 5)
+    if(argc != 5)
     {
         std::cerr <<
         "Usage: http-client-sync-ssl <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
@@ -33,7 +34,7 @@ int main(int argc, char** argv)
     auto const host = argv[1];
     auto const port = argv[2];
     auto const target = argv[3];
-    int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+    const std::string imputed_sample_filepath = argv[4];
 
     // The io_context is required for all I/O
     boost::asio::io_context ioc;
@@ -65,7 +66,7 @@ int main(int argc, char** argv)
     stream.handshake(ssl::stream_base::client);
 
     // Set up an HTTP GET request message
-    http::request<http::string_body> req{http::verb::get, target, version};
+    http::request<http::string_body> req{http::verb::get, target, 10};
     req.set(http::field::host, host);
     req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
@@ -76,15 +77,32 @@ int main(int argc, char** argv)
     boost::beast::flat_buffer buffer;
 
     // Declare a container to hold the response
-    http::response_parser<http::dynamic_body> parser;
-    parser.body_limit(1024 * 1024 * 1024);
 
     // Receive the HTTP response
+    http::response_parser<http::string_body> parser;
+    parser.body_limit(1024 * 1024 * 1024);
 
-//    std::cout << "Header done: " << parser.is_header_done() << std::endl;
-//    std::cout << "Parsed Content-Length: " << parser.content_length() << std::endl;
+    http::read_header(stream, buffer, parser);
 
-    // Write the message to standard out
+    http::read(stream, buffer, parser);
+    const http::response<http::string_body> & response = parser.get();
+
+    std::string_view response_body = response.body();
+
+    uint64_t content_length = std::stoull(response[http::field::content_length].to_string());
+
+    assert(content_length == response_body.size());
+
+    if (content_length != response_body.size()) {
+        throw std::runtime_error("content length did not match response body size");
+    }
+
+    {
+        std::ofstream output_filestream(imputed_sample_filepath);
+        output_filestream.exceptions(std::ofstream::failbit);
+        output_filestream << response.body();
+        output_filestream.close();
+    }
 
     // Gracefully close the stream
     boost::system::error_code ec;
@@ -95,16 +113,16 @@ int main(int argc, char** argv)
         // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
         ec.assign(0, ec.category());
     }
-    if(ec)
-        throw boost::system::system_error{ec};
 
-    // If we get here then the connection is closed gracefully
-    //    }
-    //    catch(std::exception const& e)
-    //    {
-    //        std::cerr << "Error: " << e.what() << std::endl;
-    //        return EXIT_FAILURE;
-    //    }
+    bool ignore_short_read_error = true;
+
+    if(ec) {
+        // Ignore short read errors because some servers are non-compliant and we cant fix them, so we need to verify above that content_length response_body.size() above
+        // https://github.com/boostorg/beast/issues/38
+        if (!(ignore_short_read_error && ec == boost::asio::ssl::error::stream_truncated)) { // ignore short read error
+            throw boost::system::system_error{ec};
+        }
+    }
     return EXIT_SUCCESS;
 }
 
